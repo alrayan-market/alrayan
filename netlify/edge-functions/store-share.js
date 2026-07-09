6b7151e64e71eddb33e09c5cf2e33e2b3afd37e8
alrayan-market <altebna@gmail.com>
2026-07-09 09:15:23 +0200

    Update store-share.js

--- netlify/edge-functions/store-share.js
+++ netlify/edge-functions/store-share.js
@@ -1,12 +1,1 @@
-[[edge_functions]]
-  path = "/p/*"
-  function = "product-share"
 
-[[edge_functions]]
-  path = "/s/*"
-  function = "store-share"
-
-[[redirects]]
-  from = "/"
-  to = "/index-23.html"
-  status = 200
