98b6225bb1460eaf04e801b0792ba3c4ed99ed13
alrayan-market <altebna@gmail.com>
2026-07-09 06:49:13 +0200

    Create store-share.js
    [[edge_functions]]
      path = "/p/*"
      function = "product-share"
    
    [[edge_functions]]
      path = "/s/*"
      function = "store-share"
    
    [[redirects]]
      from = "/"
      to = "/index-23.html"
      status = 200

---
+++ netlify/edge-functions/store-share.js
@@ -0,0 +1,12 @@
+[[edge_functions]]
+  path = "/p/*"
+  function = "product-share"
+
+[[edge_functions]]
+  path = "/s/*"
+  function = "store-share"
+
+[[redirects]]
+  from = "/"
+  to = "/index-23.html"
+  status = 200
